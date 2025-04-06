9a5f25482819c65eded16255229e5dddf1268dec
andxbes <andxbes@gmail.com>
2025-04-06 22:19:58 +0300

    236

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
-import Link from "next/link";
-
 export default function Home() {
-  // console.info('Loading');
   return (
     <main>
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href="/about">About Us</Link></p>
-      <p><Link href="/blog">Blog</Link></p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
