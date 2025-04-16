3e03b5483ea96f5c86201bf8955d869333f0f1cc
andxbes <andxbes@gmail.com>
2025-04-16 23:01:00 +0300

    443

---
+++ app/components/main-header/main-header.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+export default function MainHeader(params) {
+
+    return (
+        <>
+            <MainHeaderBackground />
+            <header className={classes.header}>
+                <Link className={classes.logo} href='/'>
+                    <Image src={logoImg} loading="eager" priority />
+                    NextLevel Food
+                </Link>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li><Link href="/meals/">Browse Meals</Link></li>
+                        <li><Link href="/community/">Foodies Community</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+};
