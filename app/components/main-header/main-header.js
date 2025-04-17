d7695188b98279eb879bfc47fd477536a311a8fb
andxbes <andxbes@gmail.com>
2025-04-17 21:37:09 +0300

    447

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 export default function MainHeader(params) {
 
     return (
@@ -10,13 +11,19 @@ export default function MainHeader(params) {
             <MainHeaderBackground />
             <header className={classes.header}>
                 <Link className={classes.logo} href='/'>
-                    <Image src={logoImg} loading="eager" priority />
+                    <Image src={logoImg} loading="eager" priority alt="logo" />
                     NextLevel Food
                 </Link>
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href="/meals/">Browse Meals</Link></li>
-                        <li><Link href="/community/">Foodies Community</Link></li>
+                        <li>
+                            <NavLink href='/meals'>Browse Meals</NavLink>
+                            {/* <Link href="/meals/" className={path.startsWith('/meals') ? classes.active : ''}>Browse Meals</Link> */}
+                        </li>
+                        <li>
+                            <NavLink href='/community'>Foodies Community</NavLink>
+                            {/* <Link href="/community/" className={path.startsWith('/community') ? classes.active : ''}>Foodies Community</Link> */}
+                        </li>
                     </ul>
                 </nav>
             </header>
